bcf4c17651c9f2ff983c3e5c41d45e2ea05add72
elpollodiablo87 <mohamedzahw@gmail.com>
2021-06-28 22:12:47 +0200

    Add files via upload
    This is the second version, made from scrach, using Intersection Observer API instead of getReactBound.

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 ## Table of Contents
 
-This is the 'Landing page project' from Udacity's web development nano degree.
+* [Instructions](#instructions)
 
-Both the HTML and CSS files were already written and needed minmal changing.
+## Instructions
 
-The project is mainly focused on bulding the navigation menu dynamically using Javascript.
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
 
-This was done using a compnation of event listener and a forEach loop.
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
--- js/app.js
+++ js/app.js
@@ -17,52 +17,68 @@
  * Define Global Variables
  * 
 */
-const frag = document.createDocumentFragment(); 
-const navList = document.getElementById('navbar__list');
-const sectionList = document.querySelectorAll('section');
-const listItem = document.querySelectorAll('li');
-const navLink = document.querySelectorAll('a');
+const navbar__list = document.getElementById('navbar__list');
+const sections_list = document.querySelectorAll('section');
+
+
 
 /**
  * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+// Function for smooth scrolling to sections when clicking nav items
+const smoothScroll = (link, section)=> {
+    link.addEventListener('click', (event)=>{
+        event.preventDefault();
+        section.scrollIntoView({
+            behavior: 'smooth'
+        })
+     })
+}
+// Function for dynamically adding listitems which contain anchor tags for navigation
+const addListItem = () => {
+    sections_list.forEach((section)=>{
+        let listItem = document.createElement('li');
+        navbar__list.append(listItem);
+        listItem.insertAdjacentHTML('afterbegin', `<a href='#${section.getAttribute('id')}'>${section.getAttribute('data-nav')}</a>`);
+        smoothScroll(listItem, section);
+    })
+}
 
+const activeClass = () => {
+    sections_list.forEach((section)=> {
+        observer = new IntersectionObserver((targets) => {
+            targets.forEach((target) => {
+              if (target.intersectionRatio > 0 ) {
+                section.classList.add('your-active-class')
+              } else {
+                section.classList.remove('your-active-class')
+              }
+            });
+          });
+          
+          observer.observe(section);
+    })
+}
+
+/**
+ * End Helper Functions
  * Begin Main Functions
  * 
 */
-
+const main = () => {
+    addListItem();
+    activeClass();
+}
+window.onload = main()
 // build the nav
-sectionList.forEach((section)=>{
-	let li = document.createElement('li');
-	let anchor = document.createElement('a');
-	anchor.textContent = section.getAttribute('data-nav');
-	anchor.href = '#'+section.getAttribute('id');
-	li.appendChild(anchor);
-	frag.appendChild(li);
-	//prevent navigation from jumping to sections
-	anchor.addEventListener('click', (evt)=> {
-		evt.preventDefault();
-		section.scrollIntoView({behavior: 'smooth'});
-	})
-})
 
-navList.appendChild(frag);
 
 // Add class 'active' to section when near top of viewport
 
-window.addEventListener("scroll", ()=> {
-	sectionList.forEach((val, index, arr)=> {
-	let rect = val.getBoundingClientRect();
-	console.log(rect);
-	if (rect.top > 0 && rect.top < 700 ) {
-		val.classList.add('your-active-class');
-		console.log('class added');
-	} else {
-		val.classList.remove('your-active-class');
-		console.log('class removed');
-	}
-})
-})
 
+// Scroll to anchor ID using scrollTO event
 
 
 /**
@@ -70,4 +86,10 @@ window.addEventListener("scroll", ()=> {
  * Begin Events
  * 
 */
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
 
